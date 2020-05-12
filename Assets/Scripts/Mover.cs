e2ea4b282fbbe8c89523a4204fd91a34acf199c4
Haojun Jiang <haojun.jiang@gdit.com>
2020-05-11 23:51:15 -0400

    Add follow up camera

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    //[SerializeField] Transform target;
 
     void Update()
     {
-        this.GetComponent<NavMeshAgent>().destination = target.position;
+        //this.GetComponent<NavMeshAgent>().destination = target.position;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
+
